042f5298bb5ec8d39ccad6644c58c8ec0757d326
Soumalya Karak <soumalyakarak2@gmail.com>
2025-11-18 13:19:56 +0530

    cuda kernal and 25-ctr added

--- gpu/AESCudaAdapter.cu
+++ gpu/AESCudaAdapter.cu
@@ -1,6 +1,11 @@
 #include "AESCudaAdapter.h"
+
 #include <cuda_runtime.h>
 #include <cstring>
+
+// Include Tezcan CUDA AES kernels
+#include "AES_final.h"
+#include "256-ctr.cuh"
 
 namespace AESCuda {
 
@@ -15,23 +20,38 @@ bool ctrEncrypt(const uint8_t* inHost, uint8_t* outHost, size_t nBytes,
 {
     if (!isAvailable() || nBytes == 0) return false;
 
-    // Allocate device buffers
-    uint8_t *dIn, *dOut;
+    // AES block size
+    size_t nBlocks = (nBytes + 15) / 16;
+
+    // Device buffers
+    uint8_t *dIn, *dOut, *dIV, *dKey;
     cudaMalloc(&dIn, nBytes);
     cudaMalloc(&dOut, nBytes);
-    cudaMemcpy(dIn, inHost, nBytes, cudaMemcpyHostToDevice);
+    cudaMalloc(&dIV, 16);
+    cudaMalloc(&dKey, 32);   // AES-256
 
-    // Call the AES kernel from whichever repo you chose
-    // Example signature from Tezcan’s repo:
-    // aes_encrypt_ctr_gpu(dOut, dIn, key, iv, nBlocks);
+    cudaMemcpy(dIn,  inHost, nBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dIV,  iv,     16,     cudaMemcpyHostToDevice);
+    cudaMemcpy(dKey, key,    32,     cudaMemcpyHostToDevice);
 
-    size_t nBlocks = (nBytes + 15) / 16;
-    aes_encrypt_ctr_gpu<<<(nBlocks+255)/256,256>>>(dOut, dIn, key, iv, nBlocks);
+    // Launch Tezcan AES-256 CTR kernel
+    aes256_ctr_encrypt_gpu<<<(nBlocks + 255) / 256, 256>>>(
+        dOut,
+        dIn,
+        dKey,
+        dIV,
+        nBlocks,
+        nBytes
+    );
 
+    cudaDeviceSynchronize();
     cudaMemcpy(outHost, dOut, nBytes, cudaMemcpyDeviceToHost);
+
     cudaFree(dIn);
     cudaFree(dOut);
-    cudaDeviceSynchronize();
+    cudaFree(dIV);
+    cudaFree(dKey);
+
     return true;
 }
 
--- gpu/AESCudaAdapter.h
+++ gpu/AESCudaAdapter.h
@@ -4,11 +4,13 @@
 
 namespace AESCuda {
 
+#ifndef __CUDACC__   // If CUDA compiler is NOT active
 inline bool isAvailable() { return false; }
 
 inline bool ctrEncrypt(const uint8_t*, uint8_t*, size_t,
                        const uint8_t*, int,
                        const uint8_t[16])
 { return false; }
+#endif
 
 }
